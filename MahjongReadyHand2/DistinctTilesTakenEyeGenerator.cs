f8c6ce12887d14eeef7977aa73fb2a1f06034c83
Opass Chang <th33ts@gmail.com>
2019-03-06 20:15:39 +0800

    add test case: only_two_same_tiles_should_return_return_collection_with_one_empty_tiles()

--- MahjongReadyHand2/DistinctTilesTakenEyeGenerator.cs
+++ MahjongReadyHand2/DistinctTilesTakenEyeGenerator.cs
@@ -1,16 +1,25 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using MahjongReadyHand2;
 
 internal class DistinctTilesTakenEyeGenerator
 {
+    private readonly IEnumerable<Tile> _tiles;
+
     public DistinctTilesTakenEyeGenerator(IEnumerable<Tile> tiles)
     {
-        
+        _tiles = tiles;
     }
 
     public IEnumerable<IEnumerable<Tile>> GetAll()
     {
+        var groups = _tiles.GroupBy(tile => tile);
+        if (groups.Count() == 1 && groups.Single().Count() == 2)
+        {
+            return new [] {Enumerable.Empty<Tile>()};
+        }
+
         return Enumerable.Empty<IEnumerable<Tile>>();
     }
 }
--- MahjongReadyHand2/DistinctTilesTakenEyeGeneratorTest.cs
+++ MahjongReadyHand2/DistinctTilesTakenEyeGeneratorTest.cs
@@ -13,5 +13,31 @@ namespace MahjongReadyHand2
             var tilesCollection = new DistinctTilesTakenEyeGenerator(Enumerable.Empty<Tile>()).GetAll();
             tilesCollection.Should().BeEmpty();
         }
+
+        [TestMethod]
+        public void tiles_without_eyes_return_empty_collection()
+        {
+            var tiles = new[]
+            {
+                new Tile("D1"),
+                new Tile("D2"),
+                new Tile("D3"),
+            };
+            var tilesCollection = new DistinctTilesTakenEyeGenerator(tiles).GetAll();
+            tilesCollection.Should().BeEmpty();
+        }
+
+        [TestMethod]
+        public void only_two_same_tiles_should_return_collection_with_one_empty_tiles()
+        {
+            var tiles = new[]
+            {
+                new Tile("D1"),
+                new Tile("D1"),
+            };
+            var tilesCollection = new DistinctTilesTakenEyeGenerator(tiles).GetAll();
+            tilesCollection.Should().ContainSingle();
+            tilesCollection.Single().Should().BeEmpty();
+        }
     }
 }
