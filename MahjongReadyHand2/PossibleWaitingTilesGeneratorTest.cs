1c432c0c5845c481f495cabd388b5855e4f33882
Opass Chang <th33ts@gmail.com>
2019-03-07 18:34:54 +0800

    refactor: extract method CalculateWaitingTiles

--- MahjongReadyHand2/PossibleWaitingTilesGeneratorTest.cs
+++ MahjongReadyHand2/PossibleWaitingTilesGeneratorTest.cs
@@ -25,35 +25,27 @@ namespace MahjongReadyHand2
         [TestMethod]
         public void tiles_themselves_are_also_possible_waiting_tiles()
         {
-            var tiles = new[]
-            {
-                new Tile("D1"),
-                new Tile("D5"),
-                new Tile("D9"),
-            };
-
-            _possibleWaitingTiles = CalculatePossibleWaitingTiles(tiles);
-            tiles.Should().BeSubsetOf(_possibleWaitingTiles);
+            CalculateWaitingTiles("D1,D5,D9");
+            _possibleWaitingTiles.Should().Contain(TileFactory.CreateTiles("D1,D5,D9"));
         }
 
         [TestMethod]
         public void possible_waiting_tiles_should_have_only_unique_tiles()
         {
-            var tiles = new[]
-            {
-                new Tile("D1"),
-                new Tile("D1"),
-            };
-
-            _possibleWaitingTiles = CalculatePossibleWaitingTiles(tiles);
+            CalculateWaitingTiles("D1,D1");
             _possibleWaitingTiles.Should().OnlyHaveUniqueItems();
         }
 
         [TestMethod]
         public void possible_waiting_tiles_should_include_sibling()
         {
-            _possibleWaitingTiles = CalculatePossibleWaitingTiles(TileFactory.CreateTiles("D2,D3"));
+            CalculateWaitingTiles("D2,D3");
             _possibleWaitingTiles.Should().Contain(TileFactory.CreateTiles("D1,D4"));
+        }
+
+        private void CalculateWaitingTiles(string tiles)
+        {
+            _possibleWaitingTiles = CalculatePossibleWaitingTiles(TileFactory.CreateTiles(tiles));
         }
     }
 }
