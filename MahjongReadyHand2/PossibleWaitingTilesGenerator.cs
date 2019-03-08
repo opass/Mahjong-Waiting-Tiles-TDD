af586fcc78c2e097538846a998ec901081c52017
Opass Chang <th33ts@gmail.com>
2019-03-08 16:10:42 +0800

    refactor: put into project namespace

--- MahjongReadyHand2/PossibleWaitingTilesGenerator.cs
+++ MahjongReadyHand2/PossibleWaitingTilesGenerator.cs
@@ -1,31 +1,32 @@
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Cryptography;
-using MahjongReadyHand2;
 
-internal static class PossibleWaitingTilesGenerator
+namespace MahjongReadyHand2
 {
-    public static IEnumerable<Tile> PossibleWaitingTiles(IEnumerable<Tile> tiles)
+    internal static class PossibleWaitingTilesGenerator
     {
-        var enumerable = tiles.ToList();
-        var quadrupletTiles = enumerable.GroupBy(tile => tile).Where(grp => grp.Count() == 4).Select(grp => grp.Key);
-
-        return enumerable.Distinct().SelectMany(tile =>
+        public static IEnumerable<Tile> PossibleWaitingTiles(IEnumerable<Tile> tiles)
         {
-            var tilesAndSibling = new List<Tile> {tile};
+            var enumerable = tiles.ToList();
+            var quadrupletTiles = enumerable.GroupBy(tile => tile).Where(grp => grp.Count() == 4).Select(grp => grp.Key);
 
-            if (tile.IsWind() || tile.IsDragon()) return tilesAndSibling;
-            if (tile.Rank != 1)
+            return enumerable.Distinct().SelectMany(tile =>
             {
-                tilesAndSibling.Add(tile.PreviousRankTile());
-            }
+                var tilesAndSibling = new List<Tile> {tile};
 
-            if (tile.Rank != 9)
-            {
-                tilesAndSibling.Add(tile.NextRankTile());
-            }
+                if (tile.IsWind() || tile.IsDragon()) return tilesAndSibling;
+                if (tile.Rank != 1)
+                {
+                    tilesAndSibling.Add(tile.PreviousRankTile());
+                }
 
-            return tilesAndSibling;
-        }).Except(quadrupletTiles);
+                if (tile.Rank != 9)
+                {
+                    tilesAndSibling.Add(tile.NextRankTile());
+                }
+
+                return tilesAndSibling;
+            }).Except(quadrupletTiles);
+        }
     }
 }
