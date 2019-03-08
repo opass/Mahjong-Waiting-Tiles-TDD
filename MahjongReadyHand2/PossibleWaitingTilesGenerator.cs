820e24f76253e78c58cc962cf763fb3ebccdbf28
Opass Chang <th33ts@gmail.com>
2019-03-08 16:19:17 +0800

    refactor: extract method CreateSelfAndSiblingTiles

--- MahjongReadyHand2/PossibleWaitingTilesGenerator.cs
+++ MahjongReadyHand2/PossibleWaitingTilesGenerator.cs
@@ -10,23 +10,25 @@ namespace MahjongReadyHand2
             var enumerable = tiles.ToList();
             var quadrupletTiles = enumerable.GroupBy(tile => tile).Where(grp => grp.Count() == 4).Select(grp => grp.Key);
 
-            return enumerable.Distinct().SelectMany(tile =>
+            return enumerable.Distinct().SelectMany(CreateSelfAndSiblingTiles ).Except(quadrupletTiles);
+        }
+
+        private static IEnumerable<Tile> CreateSelfAndSiblingTiles(Tile self)
+        {
+            var tilesAndSibling = new List<Tile> {self};
+
+            if (self.IsWind() || self.IsDragon()) return tilesAndSibling;
+            if (self.Rank != 1)
             {
-                var tilesAndSibling = new List<Tile> {tile};
+                tilesAndSibling.Add(self.PreviousRankTile());
+            }
 
-                if (tile.IsWind() || tile.IsDragon()) return tilesAndSibling;
-                if (tile.Rank != 1)
-                {
-                    tilesAndSibling.Add(tile.PreviousRankTile());
-                }
+            if (self.Rank != 9)
+            {
+                tilesAndSibling.Add(self.NextRankTile());
+            }
 
-                if (tile.Rank != 9)
-                {
-                    tilesAndSibling.Add(tile.NextRankTile());
-                }
-
-                return tilesAndSibling;
-            }).Except(quadrupletTiles);
+            return tilesAndSibling;
         }
     }
 }
