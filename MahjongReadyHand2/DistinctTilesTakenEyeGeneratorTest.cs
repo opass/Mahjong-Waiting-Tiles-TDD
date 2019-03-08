9cc25597d9cc0299c573a6090d6a277027180eda
Opass Chang <th33ts@gmail.com>
2019-03-08 15:31:51 +0800

    refactor: using GivenTiles

--- MahjongReadyHand2/DistinctTilesTakenEyeGeneratorTest.cs
+++ MahjongReadyHand2/DistinctTilesTakenEyeGeneratorTest.cs
@@ -13,7 +13,8 @@ namespace MahjongReadyHand2
         [TestMethod]
         public void no_tiles_return_empty_collection()
         {
-            var tilesCollection = new DistinctTilesTakenEyeGenerator(Enumerable.Empty<Tile>()).GetAll();
+            GivenTiles("");
+            var tilesCollection = new DistinctTilesTakenEyeGenerator(_tiles).GetAll();
             tilesCollection.Should().BeEmpty();
         }
 
@@ -33,12 +34,8 @@ namespace MahjongReadyHand2
         [TestMethod]
         public void only_two_same_tiles_should_return_collection_with_one_empty_tiles()
         {
-            var tiles = new[]
-            {
-                new Tile("D1"),
-                new Tile("D1"),
-            };
-            var tilesCollection = new DistinctTilesTakenEyeGenerator(tiles).GetAll();
+            GivenTiles("D1,D1");
+            var tilesCollection = new DistinctTilesTakenEyeGenerator(_tiles).GetAll();
             var collection = tilesCollection as IEnumerable<Tile>[] ?? tilesCollection.ToArray();
             collection.Should().ContainSingle();
             collection.Single().Should().BeEmpty();
@@ -47,20 +44,11 @@ namespace MahjongReadyHand2
         [TestMethod]
         public void D1114_should_return_collection_with_D14()
         {
-            var tiles = new[]
-            {
-                new Tile("D1"),
-                new Tile("D1"),
-                new Tile("D1"),
-                new Tile("D4"),
-            };
-            
-            var tilesCollection = new DistinctTilesTakenEyeGenerator(tiles).GetAll();
+            GivenTiles("D1,D1,D1,D4");
+            var tilesCollection = new DistinctTilesTakenEyeGenerator(_tiles).GetAll();
             var collection = tilesCollection as IEnumerable<Tile>[] ?? tilesCollection.ToArray();
             collection.Should().ContainSingle();
             collection.Single().Should().BeEquivalentTo(new Tile("D1"), new Tile("D4"));
         }
-        
-        
     }
 }
